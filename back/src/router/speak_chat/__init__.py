4e88dc7d27be6404cffc04ccd8fb6ad835a0bb9f
vyuma <bohda.yuuma.f5@s.mail.nagoya-u.ac.jp>
2026-02-08 17:45:07 +0900

    commit

--- back/src/router/speak_chat/__init__.py
+++ back/src/router/speak_chat/__init__.py
@@ -2,7 +2,7 @@ from .chat_ws import router as chat_ws_router
 from .chat import router as chat_router
 from .speakers import router as speakers_router
 from .synthesis import router as synthesis_router
-from .aizuchi import router as aizuchi_router
 from .turntaking_ws import router as turntaking_ws_router
 
 __all__ = ["chat_ws_router", "chat_router", "speakers_router", "synthesis_router", "aizuchi_router", "turntaking_ws_router"]
+    
